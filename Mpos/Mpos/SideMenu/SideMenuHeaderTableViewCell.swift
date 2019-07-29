039becdc386175acea43786aaf2c6ccb3c7ceefd
Kevin <kevin13041990@gmail.com>
2019-07-29 09:13:39 +0530

    Work on change agent on sidebar

--- Mpos/Mpos/SideMenu/SideMenuHeaderTableViewCell.swift
+++ Mpos/Mpos/SideMenu/SideMenuHeaderTableViewCell.swift
@@ -24,5 +24,16 @@ class SideMenuHeaderTableViewCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
+    
+    @IBAction func btnChangeAgentDropDownAction(_ sender: UIButton)
+    {
+        let arrtemp = ["256","257","258","259","260","261","262","263","264","265"]
+        ActionSheetStringPicker.show(withTitle: "", rows: arrtemp as [Any], initialSelection: 0, doneBlock:
+            {
+                picker, value, index in
+                self.lblAgentNumber.text = (index as? String ?? "")
+                return
+        }, cancel: { ActionStringCancelBlock in return }, origin: sender)
+    }
 
 }
